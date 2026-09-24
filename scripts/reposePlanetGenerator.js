8d54caea563d9208c837ea25aed230ade01e9ff5
Fat Bird’s Owner <182750122+Fat-Bird-Owner@users.noreply.github.com>
2026-09-24 12:37:29 +0700

    Update reposePlanetGenerator.js

--- scripts/reposePlanetGenerator.js
+++ scripts/reposePlanetGenerator.js
@@ -16,6 +16,7 @@ position.z
 
 let color = Blocks.water.mapColor
 if (depth >= 0.25) color = Blocks.grass.mapColor
+if (depth >= 0.35) color = Blocks.sand.mapColor
 if (depth >= 0.5) color = Blocks.stone.mapColor
 if (depth >= 0.75) color = Blocks.snow.mapColor
 
