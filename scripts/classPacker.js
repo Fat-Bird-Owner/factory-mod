bc19fde732bdbcbbe10d27a6e615c5acebcdcd85
Fat Bird’s Owner <182750122+Fat-Bird-Owner@users.noreply.github.com>
2026-09-24 14:52:08 +0700

    Update classPacker.js

--- scripts/classPacker.js
+++ scripts/classPacker.js
@@ -2,4 +2,5 @@ let lib = require("classLib");
 
 Events.on(ClientLoadEvent, () => {
 lib.classes.medianDrill(Vars.content.block("fs-median-drill"))
+lib.classes.overdriveCore(Vars.content.block("fs-archived-core"), 15*8);
 });
